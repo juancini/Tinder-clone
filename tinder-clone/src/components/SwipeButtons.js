3cc523e3d7094bce43e7b96345ba159a43bb16fe
Juan Mancini <juani.mancini98@gmail.com>
2021-07-25 13:06:07 -0300

    Agregado botones

--- tinder-clone/src/components/SwipeButtons.js
+++ tinder-clone/src/components/SwipeButtons.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+
+import "./SwipeButtons.css"
 
 const SwipeButtons = () => {
     return (
-        <div>
-            
+        <div className='SwipeButtons'>
+            <h1>Buttons go here</h1>
         </div>
     )
 }
